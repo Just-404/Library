6a75007055cb3ad76078ab2f94ed324ca490176c
Eddy <154628904+Just-404@users.noreply.github.com>
2024-07-19 18:27:36 -0300

    Add functionality to the library
    1. Added the logic behind the automatic creation
    of the books.
    
    2. Created a function that deletes the book
    from the library array and the page.
    
    3. Created a function that updates the data-book-position
    attribute automatically everytime a book is deleted.

--- scripts.js
+++ scripts.js
@@ -6,9 +6,100 @@ function Book(title, author, pages, read){
     this.pages = pages;
     this.read = read;
     this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ` + (this.read === true? "been read." : "not read yet.");
+       const info = `${this.title}, ${this.author}, ${this.pages},` + (this.read === 'true'? "Yesss!" : "Not yet")
+       return info.split(',');
     }
 }
+
+function addBookToLibrary(event){
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const numPages = document.querySelector("#num-pages").value;
+    const read = document.querySelector("input[name='read-option']:checked").value;
+    
+    const newBook = new Book(title, author, numPages, read);
+    library.push(newBook);
+
+    clear();
+    displayBookInfo(newBook);
+    event.preventDefault();
+}
+
+function displayBookInfo(book){
+    const main = document.querySelector("main");
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("books");
+
+    bookCard.dataset.bookPosition = "";
+
+    main.appendChild(bookCard);
+
+    const bookInfo = book.info();
+
+    const h2Array = ["Title:", "Author:", "Pages:", "Read:"]
+
+    h2Array.forEach((elem, idx) => {
+        const div = document.createElement("div");
+        const h2 = document.createElement("h2");
+        const output = document.createElement("output");
+
+        bookCard.appendChild(div);
+        h2.textContent = elem;
+
+
+        output.textContent = bookInfo[idx]   
+        div.appendChild(h2);
+        div.appendChild(output);
+
+    });  
+
+    const delDiv = document.createElement("div");
+    const delBtn = document.createElement("button");
+    delBtn.classList.add("del-button");
+    delBtn.textContent = "Delete";
+
+    
+    delBtn.addEventListener("click", (e) =>{
+        
+        let bookPos = e.target.closest("[data-book-position]");
+        deleteBook(bookPos.dataset.bookPosition);
+    });
+
+    const booksPos = document.querySelectorAll('[data-book-position]')
+    updateBookPosition(booksPos);
+    bookCard.appendChild(delDiv);
+    delDiv.appendChild(delBtn);
+    
+
+}
+
+function clear(){
+    const reset = document.querySelector("button[type=reset]");
+    reset.click();
+    dialog.close();
+}
+
+function updateBookPosition(arr){
+
+    arr.forEach((book, idx) => {
+        book.setAttribute('data-book-position', idx);
+    })
+}
+function deleteBook(bookIndex){
+    library.splice(bookIndex, 1); //fix delete
+
+
+    const del = document.querySelector(`[data-book-position="${bookIndex}"]`);  
+    del.remove();
+
+    const updateBookInfo = document.querySelectorAll("[data-book-position]");
+    updateBookPosition(updateBookInfo);
+    
+}
+const submit = document.querySelector("#add-book-btn");
+
+
+submit.addEventListener("click", addBookToLibrary, false);
 
 const dialog = document.querySelector("dialog");
 const openDialog = document.querySelector("#show-modal");
@@ -21,3 +112,23 @@ openDialog.addEventListener('click', (e) =>{
 closeDialog.addEventListener('click', (e)=>{
     dialog.close();
 });
+
+//Default books:
+
+const book1 = new Book("The Lord Of The Rings", "J.R.R Tolkien", 1216, "false");
+library.push(book1);
+displayBookInfo(book1);
+
+const book2 = new Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", 223, "false")
+library.push(book2)
+displayBookInfo(book2);
+
+const book3 = new Book("Moby-Dick", "Herman Melville", 635, "false")
+library.push(book3)
+displayBookInfo(book3);
+
+const book4 = new Book("Percy Jackson & the Olympians: The Lightning Thief", "Rick Riordan", 377, "false")
+library.push(book4)
+displayBookInfo(book4);
+
+
